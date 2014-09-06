1d0b87958d07e5858731092b3491f60a8484c930
Fabian Bull <bull.fabian@googlemail.com>
2014-09-06 14:16:21 +0200

    Added picard.sh

---
+++ picard.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -O 'picard.zip' 'http://downloads.sourceforge.net/project/picard/picard-tools/1.119/picard-tools-1.119.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpicard%2F%3Fsource%3Dtyp_redirect&ts=1409906333&use_mirror=skylink'
+
+unzip picard.zip
+rm picard.zip
+mv picard-tools-1.119 /usr/local/bin/
