8ea0f97d4ddc30b04e529adf6ac946e0049352f0
root <root@biomed1.informatik.uni-halle.de>
2015-06-17 23:09:04 +0200

    added workaround for debian specific link

--- R.sh
+++ R.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 apt-get install build-essential libreadline-dev fort77
+mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
+ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 wget http://mirrors.softliste.de/cran/src/base/R-3/R-3.1.1.tar.gz
 tar xvzf R-3.1.1.tar.gz
 rm -v R-3.1.1.tar.gz
 cd R-3.1.1
-./configure --enable-R-shlib --prefix=/usr/local/bin/R-3.1.1
+./configure --enable-R-shlib --prefix=/usr/local/bin/R-3.1.1 --with-x=no
 make
 make install
 cd ..
