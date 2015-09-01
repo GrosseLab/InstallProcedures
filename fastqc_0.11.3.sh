95c2e85b71c0de4d32e005193f4cef2ca35d1dec
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2015-09-01 08:54:39 +0200

    made upgrade easier

--- fastqc_0.11.3.sh
+++ fastqc_0.11.3.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 wget http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.3.zip
-unzip fastqc_v0.11.2.zip
-mv FastQC /usr/local/bin
+unzip fastqc_v0.11.3.zip
+mv -f FastQC /usr/local/bin
 chmod +x /usr/local/bin/FastQC/fastqc
+rm fastqc_v0.11.3.zip
+rm -r FastQC
