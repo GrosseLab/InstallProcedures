2b1d4c130bcf9dae38ad9659c8bb64075d79999d
root <root@biomed1.informatik.uni-halle.de>
2015-09-22 15:23:14 +0200

    added bedtools 2.25

---
+++ bedtools-2.25.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+## version 2.20
+
+wget https://github.com/arq5x/bedtools2/releases/download/v2.25.0/bedtools-2.25.0.tar.gz
+tar xvzf bedtools-2.25.0.tar.gz
+rm bedtools-2.25.0.tar.gz
+cd bedtools2
+make
+cd ..
+mv -v bedtools2 /usr/local/bin/bedtools2-2.25.0
