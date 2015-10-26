2b618be7194cf8aa4994afe39f867aee3f960f83
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2015-10-26 15:42:47 +0100

    Cleaned up R installation procedure.

--- R-3.2.1.sh
+++ R-3.2.1.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-apt-get install build-essential libreadline-dev fort77 liblapack-dev
-mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
-ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
+#apt-get install build-essential libreadline-dev fort77 liblapack-dev
+#mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
+#ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 wget http://mirrors.softliste.de/cran/src/base/R-3/R-3.2.1.tar.gz
 tar xvzf R-3.2.1.tar.gz
 rm -v R-3.2.1.tar.gz
 cd R-3.2.1
-./configure --enable-R-shlib --prefix=/usr/local/bin/R-3.2.1 --with-x=no --with-lapack --with-blas
+./configure --enable-R-shlib --prefix=/usr/local/bin/R-3.2.1
 make
 make install
 cd ..
