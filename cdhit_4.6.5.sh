66bc32e9f31f353a9b36a0696b82ce7aa6df84ae
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-05-12 11:25:34 +0200

    finished cdhit

--- cdhit_4.6.5.sh
+++ cdhit_4.6.5.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 wget https://github.com/weizhongli/cdhit/releases/download/V4.6.5/cd-hit-v4.6.5-2016-0304.tar.gz
-tar -xvz cd-hit-v4.6.5-2016-0304.tar.gz
+tar -xvzf cd-hit-v4.6.5-2016-0304.tar.gz
+cd cd cd-hit-v4.6.5-2016-0304/
+make
