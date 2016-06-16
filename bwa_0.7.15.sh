8673c8862bac6530c04545d6fa1a2fe90af9bb49
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-06-16 12:21:23 +0200

    added bwa 0.7.15

---
+++ bwa_0.7.15.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget -O bwa.tar.bz 'https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.15.tar.bz2/download'
+tar xvjf bwa.tar.bz
+rm bwa.tar.bz
+cd bwa-0.7.15
+make
+cd ..
+mv -v bwa-0.7.15 /usr/local/bin
+ln -s /usr/local/bin/bwa-0.7.15/bwa /usr/local/bin
