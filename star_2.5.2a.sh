d70199606dfaa185ae50a96afc166ee3f738de9d
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-08-07 22:56:56 +0200

    fixed start 2.5.2a

--- star_2.5.2a.sh
+++ star_2.5.2a.sh
@@ -1,11 +1,11 @@
-#! /bin.sh
-wget https://github.com/alexdobin/STAR/archive/STAR_2.5.2a.tar.gz
-tar -xzf STAR_2.5.2a.tar.gz
-rm STAR_2.5.2a.tar.gz
-cd STAR_2.5.2a
+#! /bin/sh
+wget https://github.com/alexdobin/STAR/archive/2.5.2a.tar.gz
+tar -xzf 2.5.2a.tar.gz
+rm 2.5.2a.tar.gz
+cd STAR-2.5.2a
 
 make STAR
 
 cd ..
-mv STAR_2.5.2a /usr/locale/bin
-ln -s /usr/locale/bin/STAR_2.5.2a/bin/STAR /usr/locale/bin/
+mv STAR-2.5.2a /usr/local/bin
+ln -s /usr/local/bin/STAR-2.5.2a/bin/STAR /usr/local/bin/
