808d4a8301b62d4cb119cf53890b9cf510e1b8e5
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-08-09 22:14:36 +0200

    fixed filename

--- blast+_2.4.0.sh
+++ blast+_2.4.0.sh
@@ -2,7 +2,7 @@
 
 wget ftp://ftp.ncbi.nlm.nih.gov//blast/executables/blast%2B/2.4.0/ncbi-blast-2.4.0%2B-x64-linux.tar.gz
 tar -xvzf "ncbi-blast-2.4.0+-x64-linux.tar.gz"
-rm "ncbi-blast-2.2.31+-x64-linux.tar.gz"
+rm "ncbi-blast-2.4.0+-x64-linux.tar.gz"
 
 mv ncbi-blast-2.4.0+ /usr/local/bin
 ln -sf /usr/local/bin/ncbi-blast-2.4.0+/bin/* /usr/local/bin/
