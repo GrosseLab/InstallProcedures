cab7cf4d9ef3f79adf32b695d767323957d2f05e
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-08-09 13:55:40 +0200

    fixed missing line

--- segemehl_0.2.0.sh
+++ segemehl_0.2.0.sh
@@ -7,5 +7,6 @@ make
 cd ../..
 rm segemehl_0_2_0.tar.gz
 mv segemehl_0_2_0/segemehl /usr/local/bin/segemehl-0.2.0
+rm -rf segemehl_0_2_0
 
 ln -sf /usr/local/bin/segemehl_0_2_0/segemehl.x /usr/local/bin/
