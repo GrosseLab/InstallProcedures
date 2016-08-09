cf97a675fc1076970fe5e57357972f7e79c974ea
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-08-09 14:39:19 +0200

    path fix

--- segemehl_0.2.0.sh
+++ segemehl_0.2.0.sh
@@ -9,4 +9,4 @@ rm segemehl_0_2_0.tar.gz
 mv segemehl_0_2_0/segemehl /usr/local/bin/segemehl-0.2.0
 rm -rf segemehl_0_2_0
 
-ln -sf /usr/local/bin/segemehl_0_2_0/segemehl.x /usr/local/bin/
+ln -sf /usr/local/bin/segemehl-0.2.0/segemehl.x /usr/local/bin/
