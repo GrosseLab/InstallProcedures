8a749a5b420067266825e8b74c927d1013b5caf8
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-09-07 15:02:10 +0200

    picard 2.6.0

--- picard_2.6.0.sh
+++ picard_2.6.0.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-wget --quiet 'https://github.com/broadinstitute/picard/releases/download/2.6.0/picard-tools-2.6.0.zip'
-unzip picard-tools-2.6.0.zip
-rm picard-tools-2.6.0.zip
-mv picard-tools-2.6.0 /usr/local/bin/
+wget --quiet 'https://github.com/broadinstitute/picard/releases/download/2.6.0/picard.jar'
+mkdir /usr/local/bin/picard-tools-2.6.0
+mv  picard.jar /usr/local/bin/
