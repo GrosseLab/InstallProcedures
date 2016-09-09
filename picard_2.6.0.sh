2999e2ff4b97d5d327fc0072389aa26f42e12234
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2016-09-09 09:54:37 +0200

    path fix

--- picard_2.6.0.sh
+++ picard_2.6.0.sh
@@ -2,4 +2,4 @@
 
 wget --quiet 'https://github.com/broadinstitute/picard/releases/download/2.6.0/picard.jar'
 mkdir /usr/local/bin/picard-tools-2.6.0
-mv  picard.jar /usr/local/bin/
+mv  picard.jar /usr/local/bin/picard-tools-2.6.0
